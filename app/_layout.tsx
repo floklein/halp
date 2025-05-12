2465f9d0972947f1e982031e65537ee1739f7c40
floklein <4128942+floklein@users.noreply.github.com>
2025-05-12 19:53:30 +0200

    fixed bg

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,10 +9,11 @@ import { useState } from "react";
 import { StatusBar, useColorScheme } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Button, H1, View, XStack, YStack } from "tamagui";
+import { Button, H1, useTheme, View, XStack, YStack } from "tamagui";
 
-export default function Layout() {
+function LayoutWithinProviders() {
   const colorScheme = useColorScheme();
+  const theme = useTheme();
 
   const [newOpen, setNewOpen] = useState(false);
 
@@ -21,48 +22,54 @@ export default function Layout() {
   }
 
   return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView style={{ flex: 1, backgroundColor: theme.background.val }}>
+        <StatusBar
+          barStyle={colorScheme === "dark" ? "light-content" : "dark-content"}
+        />
+        <YStack flex={1}>
+          <XStack justify="center" py="$2">
+            <H1 fontWeight="bold">HALP!</H1>
+          </XStack>
+          <View flex={1} style={{ zIndex: 1 }}>
+            <Slot />
+          </View>
+          <XStack items="flex-end" pt="$2" style={{ zIndex: 2 }}>
+            <Link href="/" asChild>
+              <Button flex={1} height="$6" rounded="$0">
+                <FlipHorizontal size="$2" />
+              </Button>
+            </Link>
+            <ProtectedButton
+              onPress={openNew}
+              height="$7"
+              width="$7"
+              theme="accent"
+              fontSize="$8"
+              fontWeight="bold"
+              px="$0"
+            >
+              ask
+            </ProtectedButton>
+            <Link href="/account" asChild>
+              <Button flex={1} height="$6" rounded="$0">
+                <UserCircle size="$2" />
+              </Button>
+            </Link>
+          </XStack>
+          <AskSheet open={newOpen} setOpen={setNewOpen} />
+          <CurrentToast />
+          <ToastViewport width="100%" />
+        </YStack>
+      </SafeAreaView>
+    </GestureHandlerRootView>
+  );
+}
+
+export default function Layout() {
+  return (
     <Providers>
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <SafeAreaView style={{ flex: 1 }}>
-          <StatusBar
-            barStyle={colorScheme === "dark" ? "light-content" : "dark-content"}
-          />
-          <YStack flex={1}>
-            <XStack justify="center" py="$2">
-              <H1 fontWeight="bold">HALP!</H1>
-            </XStack>
-            <View flex={1} style={{ zIndex: 1 }}>
-              <Slot />
-            </View>
-            <XStack items="flex-end" pt="$2" style={{ zIndex: 2 }}>
-              <Link href="/" asChild>
-                <Button flex={1} height="$6" rounded="$0">
-                  <FlipHorizontal size="$2" />
-                </Button>
-              </Link>
-              <ProtectedButton
-                onPress={openNew}
-                height="$7"
-                width="$7"
-                theme="accent"
-                fontSize="$8"
-                fontWeight="bold"
-                px="$0"
-              >
-                ask
-              </ProtectedButton>
-              <Link href="/account" asChild>
-                <Button flex={1} height="$6" rounded="$0">
-                  <UserCircle size="$2" />
-                </Button>
-              </Link>
-            </XStack>
-            <AskSheet open={newOpen} setOpen={setNewOpen} />
-            <CurrentToast />
-            <ToastViewport width="100%" />
-          </YStack>
-        </SafeAreaView>
-      </GestureHandlerRootView>
+      <LayoutWithinProviders />
     </Providers>
   );
 }
